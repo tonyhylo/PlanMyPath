28122eb56ce139fcf72f806ef2403fede2b3cf1a
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-05-12 00:21:56 -0400

    Removed duplicates from findpathspage

--- src/pages/FindPathsPage/FindPathsPage.jsx
+++ src/pages/FindPathsPage/FindPathsPage.jsx
@@ -12,11 +12,29 @@ export default function FindPathsPage(props) {
     setAllPaths(paths);
   }, []);
 
+  // useEffect(() => {
+  //   const tempTags = [];
+  //   allPaths.map((keys, i) => {
+  //     keys.tags.map((tag, t) => {
+  //       tempTags.push(tag);
+  //     })
+  //   });
+  //   setAllTags(tempTags);
+  // }, [allPaths]);
+
   useEffect(() => {
     const tempTags = [];
     allPaths.map((keys, i) => {
       keys.tags.map((tag, t) => {
-        tempTags.push(tag);
+        let tagExists = 0;
+        for (let t = 0; t < tempTags.length; t++) {
+          if (tempTags[t] == tag) {
+            tagExists = tagExists + 1;
+          }
+        }
+        if (!tagExists) {
+          tempTags.push(tag);
+        }
       })
     });
     setAllTags(tempTags);
