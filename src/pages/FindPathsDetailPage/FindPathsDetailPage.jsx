a4ad3e0448d6ef448d5ccee7407869dca526a112
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-05-12 01:08:41 -0400

    Added findpath by country

--- src/pages/FindPathsDetailPage/FindPathsDetailPage.jsx
+++ src/pages/FindPathsDetailPage/FindPathsDetailPage.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import * as userService from '../../utilities/users-service'
+import { useEffect, useState } from "react";
+import * as userService from "../../utilities/users-service";
 import { useParams, useNavigate } from "react-router-dom";
-import ItineraryCard from '../ItineraryCard/ItineraryCard';
+import ItineraryCard from "../ItineraryCard/ItineraryCard";
 
 export default function FindPathsDetailPage(props) {
   const { findPathName } = useParams();
@@ -24,26 +24,32 @@ export default function FindPathsDetailPage(props) {
         return null;
       });
     }
+    if (tempFoundPaths.length < 1) {
+      console.log("doing country");
+      props.myPaths.forEach((path) => {
+        if (findPathName == path.country) {
+          tempFoundPaths.push(path);
+        }
+        return null;
+      });
+    }
     setFoundPaths(tempFoundPaths);
-    console.log(1, tempFoundPaths);
-  }, [])
-
+  }, []);
 
   return (
     <>
       <h1>FindPathsDetailPage</h1>
-      {foundPaths.map((path, i) =>
-            <>
-              <ItineraryCard
-                country={path.country}
-                tags={path.tags}
-                description={path.description}
-                title={path.title}
-                user={path.user}
-              />
-            </>
-          )
-        }
+      {foundPaths.map((path, i) => (
+        <>
+          <ItineraryCard
+            country={path.country}
+            tags={path.tags}
+            description={path.description}
+            title={path.title}
+            user={path.user}
+          />
+        </>
+      ))}
     </>
   );
 }
--- src/pages/FindPathsPage/FindPathsPage.jsx
+++ src/pages/FindPathsPage/FindPathsPage.jsx
@@ -5,22 +5,12 @@ import TagsListItem from "../TagsListItem/TagsListItem";
 export default function FindPathsPage(props) {
   const [allPaths, setAllPaths] = useState([]);
   const [allTags, setAllTags] = useState([]);
+  const [allCountries, setAllCountries] = useState([]);
 
   useEffect(() => {
     const paths = props.myPaths;
-    // console.log(paths);
     setAllPaths(paths);
   }, []);
-
-  // useEffect(() => {
-  //   const tempTags = [];
-  //   allPaths.map((keys, i) => {
-  //     keys.tags.map((tag, t) => {
-  //       tempTags.push(tag);
-  //     })
-  //   });
-  //   setAllTags(tempTags);
-  // }, [allPaths]);
 
   useEffect(() => {
     const tempTags = [];
@@ -35,19 +25,41 @@ export default function FindPathsPage(props) {
         if (!tagExists) {
           tempTags.push(tag);
         }
-      })
+      });
     });
     setAllTags(tempTags);
+  }, [allPaths]);
+
+  useEffect(() => {
+    const tempCountries = [];
+    allPaths.map((keys, i) => {
+      let countryExists = 0;
+      for (let t = 0; t < tempCountries.length; t++) {
+        if (tempCountries[t] == keys.country) {
+          countryExists = countryExists + 1;
+        }
+      }
+      if (!countryExists) {
+        tempCountries.push(keys.country);
+      }
+    });
+    setAllCountries(tempCountries);
   }, [allPaths]);
 
   return (
     <>
       <h1>FindPathsPage</h1>
+      <div>By Tag:</div>
       <ul>
         {allTags.map((tag1, i) => {
-          return <TagsListItem tagsItem={tag1} title="title6" />
+          return <TagsListItem tagsItem={tag1} title="title6" />;
         })}
-
+      </ul>
+      <div>By Country:</div>
+      <ul>
+        {allCountries.map((tag1, i) => {
+          return <TagsListItem tagsItem={tag1} title="title6" />;
+        })}
       </ul>
     </>
   );
