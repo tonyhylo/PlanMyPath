e3fe6b53abdc9c45124dcfbb64ca87175e0ea8e0
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-05-12 00:47:11 -0400

    Added findpaths functionality, search by tag

--- src/pages/FindPathsDetailPage/FindPathsDetailPage.jsx
+++ src/pages/FindPathsDetailPage/FindPathsDetailPage.jsx
@@ -1,29 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as userService from '../../utilities/users-service'
 import { useParams, useNavigate } from "react-router-dom";
+import ItineraryCard from '../ItineraryCard/ItineraryCard';
 
 export default function FindPathsDetailPage(props) {
   const { findPathName } = useParams();
+  const [foundPaths, setFoundPaths] = useState([]);
   // let thesePaths = props.myPaths.find((path) => path.title === findPathName);
-  let thesePaths = props.myPaths.map((path) => {
-    path.tags.find((tag) => {
-      return tag === findPathName;
-    })
-  })
+  // let thesePaths = props.myPaths.map((path) => {
+  //   path.tags.find((tag) => {
+  //     return tag === findPathName;
+  //   })
+  // })
 
   useEffect(() => {
-    // props.myPaths.map((path, t) => {
-    //   path.tags.map((tag, n) => {
-    //     console.log(tag);
-    //   })
-    // })
-    console.log(1, thesePaths);
+    let tempFoundPaths = [];
+    for (let t = 0; t < props.myPaths.length; t++) {
+      // console.log(props.myPaths[t].tags);
+      props.myPaths[t].tags.forEach((tag) => {
+        if (findPathName == tag) {
+          tempFoundPaths.push(props.myPaths[t]);
+        }
+        return null;
+      });
+    }
+    setFoundPaths(tempFoundPaths);
+    console.log(1, tempFoundPaths);
   }, [])
 
 
   return (
     <>
       <h1>FindPathsDetailPage</h1>
+      {foundPaths.map((path, i) =>
+            <>
+              <ItineraryCard
+                country={path.country}
+                tags={path.tags}
+                description={path.description}
+                title={path.title}
+                user={path.user}
+              />
+            </>
+          )
+        }
     </>
   );
 }
